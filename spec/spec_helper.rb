134f6ea8cc0f837ef1f25b4b25c74ce3377d12a7
Andy Delcambre <adelcambre@engineyard.com>
2010-01-18 11:05:05 -0800

    Remove 'cloud' cruft from specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'cloud'
+require 'engineyard'
 require 'spec'
 require 'spec/autorun'
 
