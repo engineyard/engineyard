89174ff1cae521c48d0ba799efe6da5608a62513
Andy Delcambre <adelcambre@engineyard.com>
2010-01-18 11:07:58 -0800

    Make rake work with bundler

--- Gemfile
+++ Gemfile
@@ -3,5 +3,9 @@ only :test do
   gem "rspec", :require_as => "spec"
 end
 
+only :rake do
+  gem "jeweler"
+end
+
 disable_system_gems
 bin_path "gbin"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'engineyard'
+
+Bundler.require_env :test
+
 require 'spec'
 require 'spec/autorun'
 
