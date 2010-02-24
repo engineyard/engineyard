765a18d312adfe0d223d4cb8b84c918c009b1d79
Andre Arko <aarko@engineyard.com>
2010-02-24 15:24:53 -0800

    Deploy task now remote-installs ey-deploy gem and runs eysd deploy
    Change-Id: Ifd4e60c000666fb6a445ca31f9ba96d76fc69fdd
    Reviewed-on: http://review.engineyard.com/73
    Reviewed-by: Andy Delcambre <adelcambre@engineyard.com>
    Tested-by: Andy Delcambre <adelcambre@engineyard.com>

--- lib/engineyard/cli.rb
+++ lib/engineyard/cli.rb
@@ -17,24 +17,55 @@ module EY
     desc "deploy [ENVIRONMENT] [BRANCH]", "Deploy [BRANCH] of the app in the current directory to [ENVIRONMENT]"
     method_option :force, :type => :boolean, :aliases => %w(-f), :desc => "Force a deploy of the specified branch"
     method_option :migrate, :type => :boolean, :default => true, :aliases => %w(-m), :desc => "Run migrations after deploy"
+    method_option :install_eysd, :type => :boolean, :aliases => %(-s), :desc => "Force remote install of eysd"
     def deploy(env_name = nil, branch = nil)
       env_name ||= config.default_environment
+      raise RequiredArgumentMissingError, "[ENVIRONMENT] not provided" unless env_name
+
       default_branch = config.default_branch(env_name)
       branch ||= (default_branch || repo.current_branch)
+      raise RequiredArgumentMissingError, "[BRANCH] not provided" unless branch
 
       if default_branch && (branch != default_branch) && !options[:force]
         raise BranchMismatch, %{Your deploy branch is set to "#{default_branch}".\n} +
         %{If you want to deploy branch "#{branch}", use --force.}
       end
 
-      env = account.environments.find{|e| e["name"] == env_name }
+      app = account.app_for_url(repo.url)
+      env = app["environments"].find{|e| e["name"] == env_name }
       raise EnvironmentError, "No environment named '#{env_name}' running this app" unless env
 
       # OMG EY cloud quotes nulls when it returns JSON :(
       app_master = env["app_master"] != "null" && env["app_master"]
-      raise EnvironmentError, "Your environment isn't running" unless app_master
+      raise EnvironmentError, "No running app master" unless app_master && app_master["status"] == "running"
+      ip = app_master["ip_address"]
 
-      puts "ssh #{env["app_master"]} eysd update --app --branch #{branch}"
+      EY.ui.info "Connecting to the server..."
+      eysd_installed = ssh(ip, "which eysd", false)
+      raise EnvironmentError, "SSH connection to #{ip} failed" if $?.exitstatus == 255
+
+      if !eysd_installed || options[:install_eysd]
+        EY.ui.info "Installing ey-deploy gem..."
+        gem_file = Dir["ey-deploy*.gem"].first
+        if gem_file
+          system %{scp #{gem_file} root@#{ip}:~/}
+        else
+          raise EY::Error "Could not find ey-deploy gem file in the current directory"
+        end
+        ssh(ip, "gem install #{gem_file}")
+      end
+
+      deploy_cmd = "eysd update --app #{app["name"]} --branch #{branch}"
+      if options[:migrate]
+        deploy_cmd << " --migrate"
+      else
+        deploy_cmd << " --no-migration-command"
+      end
+
+      EY.ui.info "Running deploy on server..."
+      ssh(ip, deploy_cmd)
+
+      EY.ui.info "Deploy complete."
     end
 
 
@@ -78,6 +109,17 @@ module EY
 
     def config
       @config ||= EY::Config.new
+    end
+
+    def debug(*args)
+      EY.ui.debug(*args)
+    end
+
+    def ssh(ip, remote_cmd, output = true)
+      cmd = %{ssh root@#{ip} "#{remote_cmd}"}
+      cmd << %{ &> /dev/null} unless output
+      puts cmd if output
+      system cmd
     end
 
     def print_envs(envs)
