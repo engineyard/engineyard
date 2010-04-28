6ad84a3795860243aec0f31d96634dfc13a262b4
Andy Delcambre <adelcambre@engineyard.com>
2010-04-28 16:31:52 -0700

    Remove pre for release
    Change-Id: I4befc8594248cc8e6344e9d21beafd572aa6a355
    Reviewed-on: http://review.engineyard.com/229
    Reviewed-by: Andy Delcambre <adelcambre@engineyard.com>
    Tested-by: Andy Delcambre <adelcambre@engineyard.com>

--- lib/engineyard.rb
+++ lib/engineyard.rb
@@ -1,5 +1,5 @@
 module EY
-  VERSION = "0.2.11.pre"
+  VERSION = "0.2.11"
 
   autoload :Account, 'engineyard/account'
   autoload :API,     'engineyard/api'
--- lib/engineyard/cli.rb
+++ lib/engineyard/cli.rb
@@ -4,7 +4,7 @@ require 'engineyard/cli/error'
 
 module EY
   class CLI < Thor
-    EYSD_VERSION = "~>0.2.5"
+    EYSD_VERSION = "~>0.2.6"
 
     autoload :API, 'engineyard/cli/api'
     autoload :UI,  'engineyard/cli/ui'
