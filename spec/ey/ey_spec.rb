72364878f4d78468a85ca5db09e2997ab2353eb5
Sam Merritt <smerritt@engineyard.com>
2010-04-28 17:26:36 -0700

    Add parentheses to spec to silence warnings
    Change-Id: If968a25f07ab1694c080cba70b53f458f6b2111c
    Reviewed-on: http://review.engineyard.com/233
    Reviewed-by: Andre Arko <aarko@engineyard.com>
    Tested-by: Andre Arko <aarko@engineyard.com>

--- spec/ey/ey_spec.rb
+++ spec/ey/ey_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe "ey" do
   context "run without arguments" do
     it "prints usage information" do
-      ey.should include "Tasks:"
+      ey.should include("Tasks:")
     end
   end
 
   context "run with an argument that is not a command" do
     it "tells the user that is not a command" do
       ey "foobarbaz", :hide_err => true
-      @err.should include "Could not find task"
+      @err.should include("Could not find task")
     end
   end
 end
