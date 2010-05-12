6c7d0cdaf91fae3335e75f218fdd2dbf0322d001
Sam Merritt <smerritt@engineyard.com>
2010-05-11 17:16:57 -0700

    Inline EY::Action classes into EY::CLI for small actions.
    Change-Id: I36fa68415d74512001aa72ebd65573703eb1be8c
    Reviewed-on: http://review.engineyard.com/256
    Reviewed-by: Andre Arko <aarko@engineyard.com>
    Tested-by: Andre Arko <aarko@engineyard.com>

--- lib/engineyard/action/util.rb
+++ lib/engineyard/action/util.rb
@@ -14,34 +14,6 @@ module EY
         @repo ||= EY::Repo.new
       end
 
-      def app_and_envs(all_envs = false)
-        app = account.app_for_repo(repo)
-
-        if all_envs || !app
-          envs = account.environments
-          EY.ui.warn(NoAppError.new(repo).message) unless app || all_envs
-          [nil, envs]
-        else
-          envs = app.environments
-          if envs.empty?
-            EY.ui.warn %|You have no environments set up for the application "#{app.name}"|
-              EY.ui.warn %|You can make one at #{EY.config.endpoint}|
-          end
-          envs.empty? ? [app, nil] : [app, envs]
-        end
-      end
-
-      def env_named(name)
-        env = account.environment_named(name)
-
-        if env.nil?
-          raise EnvironmentError, "Environment '#{env_name}' can't be found\n" +
-            "You can create it at #{EY.config.endpoint}"
-        else
-          env
-        end
-      end
-
     end
   end
 end
--- lib/engineyard/cli.rb
+++ lib/engineyard/cli.rb
@@ -32,8 +32,16 @@ module EY
     desc "environments [--all]", "List cloud environments for this app, or all environments"
     method_option :all, :type => :boolean, :aliases => %(-a)
     def environments
-      require 'engineyard/action/list_environments'
-      EY::Action::ListEnvironments.call(options[:all])
+      app, envs = app_and_envs(options[:all])
+      if app
+        EY.ui.say %|Cloud environments for #{app.name}:|
+          EY.ui.print_envs(envs, EY.config.default_environment)
+      elsif envs
+        EY.ui.say %|Cloud environments:|
+          EY.ui.print_envs(envs, EY.config.default_environment)
+      else
+        EY.ui.say %|You do not have any cloud environments.|
+      end
     end
     map "envs" => :environments
 
@@ -45,20 +53,38 @@ module EY
 
     desc "ssh ENV", "Open an ssh session to the environment's application server"
     def ssh(name)
-      require 'engineyard/action/ssh'
-      EY::Action::SSH.call(name)
+      env = account.environment_named(name)
+      if env
+        Kernel.exec "ssh", "#{env.username}@#{env.app_master.public_hostname}", *ARGV[2..-1]
+      else
+        EY.ui.warn %|Could not find an environment named "#{name}"|
+      end
     end
 
     desc "logs ENV", "Retrieve the latest logs for an enviornment"
     def logs(name)
-      require 'engineyard/action/show_logs'
-      EY::Action::ShowLogs.call(name)
+      env_named(name).logs.each do |log|
+        EY.ui.info log.instance_name
+
+        if log.main
+          EY.ui.info "Main logs:"
+          EY.ui.say  log.main
+        end
+
+        if log.custom
+          EY.ui.info "Custom logs:"
+          EY.ui.say  log.custom
+        end
+      end
     end
 
     desc "upload_recipes ENV", "Upload custom chef recipes from the current directory to ENV"
     def upload_recipes(name)
-      require 'engineyard/action/upload_recipes'
-      EY::Action::UploadRecipes.call(name)
+      if account.upload_recipes_for(env_named(name))
+        EY.ui.say "Recipes uploaded successfully"
+      else
+        EY.ui.error "Recipes upload failed"
+      end
     end
 
     desc "version", "Print the version of the engineyard gem"
@@ -66,6 +92,43 @@ module EY
       EY.ui.say %{engineyard version #{EY::VERSION}}
     end
     map "-v" => :version
+
+    private
+    def account
+      @account ||= EY::Account.new(EY::CLI::API.new)
+    end
+
+    def repo
+      @repo ||= EY::Repo.new
+    end
+
+    def env_named(env_name)
+      env = account.environment_named(env_name)
+
+      if env.nil?
+        raise EnvironmentError, "Environment '#{env_name}' can't be found\n" +
+          "You can create it at #{EY.config.endpoint}"
+      end
+
+      env
+    end
+
+    def app_and_envs(all_envs = false)
+      app = account.app_for_repo(repo)
+
+      if all_envs || !app
+        envs = account.environments
+        EY.ui.warn(NoAppError.new(repo).message) unless app || all_envs
+        [nil, envs]
+      else
+        envs = app.environments
+        if envs.empty?
+          EY.ui.warn %|You have no environments set up for the application "#{app.name}"|
+            EY.ui.warn %|You can make one at #{EY.config.endpoint}|
+        end
+        envs.empty? ? [app, nil] : [app, envs]
+      end
+    end
 
   end # CLI
 end # EY
