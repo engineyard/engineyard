e39b40b6194fe50d0a314680493d3500df4f4fe4
Sam Merritt <smerritt@engineyard.com>
2010-06-01 17:58:09 -0700

    Make "ey upload_recipes" able to infer the environment from the current application.
    [#73]
    
    Change-Id: I878cb7dcd009c61b4e2ccf4468b4a33af5b5aa4a
    Reviewed-on: http://review.engineyard.com/301
    Reviewed-by: Martin Emde <memde@engineyard.com>
    Tested-by: Martin Emde <memde@engineyard.com>

--- lib/engineyard/cli/recipes.rb
+++ lib/engineyard/cli/recipes.rb
@@ -1,9 +1,9 @@
 module EY
   class CLI
     class Recipes < EY::Thor
-      desc "recipes upload ENV", "Upload custom chef recipes from the current directory to ENV"
-      def upload(name)
-        if api.environments.match_one!(name).upload_recipes
+      desc "recipes upload [ENV]", "Upload custom chef recipes from the current directory to ENV"
+      def upload(name = nil)
+        if fetch_environment(name).upload_recipes
           EY.ui.say "Recipes uploaded successfully"
         else
           EY.ui.error "Recipes upload failed"
--- spec/ey/upload_recipes_spec.rb
+++ spec/ey/upload_recipes_spec.rb
@@ -3,16 +3,22 @@ require 'spec_helper'
 describe "ey recipes upload" do
   it_should_behave_like "an integration test"
 
-  it "posts the recipes to the correct url" do
-    api_scenario "one app, one environment"
-    dir = Pathname.new("/tmp/#{$$}")
-    dir.mkdir
-    Dir.chdir(dir) do
-      dir.join("cookbooks").mkdir
-      File.open(dir.join("cookbooks/file"), "w"){|f| f << "boo" }
+  before(:all) do
+    @recipe_dir = Pathname.new("/tmp/#{$$}")
+    @recipe_dir.mkdir
+    Dir.chdir(@recipe_dir) do
+      @recipe_dir.join("cookbooks").mkdir
+      File.open(@recipe_dir.join("cookbooks/file"), "w"){|f| f << "boo" }
       `git init`
       `git add .`
       `git commit -m "OMG"`
+      `git remote add testremote user@host.tld:path/to/repo.git`
+    end
+  end
+
+  it "posts the recipes to the correct url" do
+    api_scenario "one app, one environment"
+    Dir.chdir(@recipe_dir) do
       ey "recipes upload giblets", :debug => true
     end
 
@@ -24,5 +30,25 @@ describe "ey recipes upload" do
     ey "recipes upload bogusenv", :expect_failure => true
 
     @err.should =~ /can't be found/i
+  end
+
+  it "can infer the environment from the current application" do
+    api_scenario "one app, one environment", "user@host.tld:path/to/repo.git"
+
+    Dir.chdir(@recipe_dir) do
+      ey "recipes upload", :debug => true
+    end
+
+    @out.should =~ /recipes uploaded successfully/i
+  end
+
+  it "complains when it can't infer the environment from the current application" do
+    api_scenario "one app, one environment, not linked", "user@host.tld:path/to/repo.git"
+
+    Dir.chdir(@recipe_dir) do
+      ey "recipes upload", :debug => true, :expect_failure => true
+    end
+
+    @err.should =~ /single environment/i
   end
 end
