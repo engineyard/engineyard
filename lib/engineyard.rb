7722451fb4507959b9c4405aa56503cd49dc5f88
Sam Merritt <smerritt@engineyard.com>
2010-06-03 15:39:29 -0700

    Add .pre for next version
    Change-Id: Ibbd5b122c4856bb1ab28cd81d6a39ea51da6ca8d

--- lib/engineyard.rb
+++ lib/engineyard.rb
@@ -1,7 +1,7 @@
 module EY
   require 'engineyard/ruby_ext'
 
-  VERSION = "0.3.3"
+  VERSION = "0.3.4.pre"
 
   autoload :API,        'engineyard/api'
   autoload :Collection, 'engineyard/collection'
