6e67d9152163b1442c09a5b1f9b3bf7fed0e3c8b
Sam Merritt <smerritt@engineyard.com>
2010-06-10 15:11:06 -0700

    Simplify test for `ey web enable/disable`.
    [#89]

--- spec/ey/web/disable_spec.rb
+++ spec/ey/web/disable_spec.rb
@@ -3,27 +3,12 @@ require 'spec_helper'
 describe "ey web disable" do
   given "integration"
 
-  use_git_repo('default')
-
-  before(:all) do
-    api_scenario "one app, one environment"
-  end
-
-  it "tells eysd to put up the maintenance page" do
-    ey "web disable"
-    @ssh_commands.should have_command_like(/eysd deploy enable_maintenance_page --app rails232app/)
-  end
-end
-
-describe "ey web disable" do
-  given "integration"
-
   def command_to_run(opts)
     "web disable #{opts[:env]}"
   end
 
   def verify_ran(scenario)
-    @out.should =~ /Putting up maintenance page.*#{scenario[:environment]}/
+    @ssh_commands.should have_command_like(/eysd deploy enable_maintenance_page --app #{scenario[:application]}/)
   end
 
   it_should_behave_like "it takes an environment name"
--- spec/ey/web/enable_spec.rb
+++ spec/ey/web/enable_spec.rb
@@ -2,27 +2,13 @@ require 'spec_helper'
 
 describe "ey web enable" do
   given "integration"
-  use_git_repo('default')
-
-  before(:all) do
-    api_scenario "one app, one environment"
-  end
-
-  it "tells eysd to take down the maintenance page" do
-    ey "web enable"
-    @ssh_commands.should have_command_like(/eysd deploy disable_maintenance_page --app rails232app/)
-  end
-end
-
-describe "ey web enable" do
-  given "integration"
 
   def command_to_run(opts)
     "web enable #{opts[:env]}"
   end
 
   def verify_ran(scenario)
-    @out.should =~ /Taking down maintenance page.*#{scenario[:environment]}/
+    @ssh_commands.should have_command_like(/eysd deploy disable_maintenance_page --app #{scenario[:application]}/)
   end
 
   it_should_behave_like "it takes an environment name"
