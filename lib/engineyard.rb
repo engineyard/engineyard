8dbde77578d2a12f8fcf3208d7a4aec5c929ecd1
Sam Merritt <smerritt@engineyard.com>
2010-06-17 12:30:18 -0700

    Bump versions for release 0.5.1

--- lib/engineyard.rb
+++ lib/engineyard.rb
@@ -1,7 +1,7 @@
 module EY
   require 'engineyard/ruby_ext'
 
-  VERSION = "0.5.1.pre"
+  VERSION = "0.5.1"
 
   autoload :API,        'engineyard/api'
   autoload :Collection, 'engineyard/collection'
