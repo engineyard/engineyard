6723d2bd24ada793b9c28c81856bffe6610a57d0
Sam Merritt <smerritt@engineyard.com>
2010-07-22 11:50:58 -0700

    Unvendored thor and switched to thor 0.13.8.
    The new thor has all the subcommand stuff in it, plus print_table
    takes :colwidth. That's all of our pending patches.
    
    Removed now-redundant code from EY::Thor.
    
    Moved utility methods in EY::Thor into a module to make it easier to
    distinguish between helper methods for commands and Thor tweaks.
    
    [#164]

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source :gemcutter
 
+gem "thor",        "~>0.13.8"
 gem "rest-client", "~>1.4", :require => "rest_client"
 gem "highline",    '~>1.5.2'
 gem "termios"
--- lib/engineyard/thor.rb
+++ lib/engineyard/thor.rb
@@ -1,74 +1,8 @@
-$LOAD_PATH.unshift File.expand_path("../vendor", __FILE__)
 require 'thor'
 
 module EY
-  class Thor < ::Thor
-    def self.start(original_args=ARGV, config={})
-      @@original_args = original_args
-      super
-    end
-
-    no_tasks do
-      def self.subcommands
-        @@subcommands ||= {}
-      end
-
-      def self.subcommand(subcommand, subcommand_class)
-        subcommand = subcommand.to_s
-        subcommands[subcommand] = subcommand_class
-        subcommand_class.subcommand_help subcommand
-        define_method(subcommand) { |*_| subcommand_class.start(subcommand_args) }
-      end
-
-      def self.subcommand_help(cmd)
-        desc "#{cmd} help [COMMAND]", "Describe all subcommands or one specific subcommand."
-        class_eval <<-RUBY
-          def help(*args)
-            if args.empty?
-              EY.ui.say "usage: #{banner_base} #{cmd} COMMAND"
-              EY.ui.say
-              subcommands = self.class.printable_tasks.sort_by{|s| s[0] }
-              subcommands.reject!{|t| t[0] =~ /#{cmd} help$/}
-              EY.ui.print_help(subcommands)
-              EY.ui.say self.class.send(:class_options_help, EY.ui)
-              EY.ui.say "See #{banner_base} #{cmd} help COMMAND" +
-                " for more information on a specific subcommand." if args.empty?
-            else
-              super
-            end
-          end
-        RUBY
-      end
-
-      def subcommand_args
-        @@original_args[1..-1]
-      end
-
-      def self.banner_base
-        "ey"
-      end
-
-      def self.banner(task, task_help = false)
-        scmd = EY::Thor.subcommands.invert[self]
-        task = (task_help ? task.formatted_usage(self, false) : task.name)
-        [banner_base, scmd, task].compact.join(" ")
-      end
-    end
-
+  module UtilityMethods
     protected
-
-    def self.handle_no_task_error(task)
-      if self.banner_base == "thor"
-        raise UndefinedTaskError, "Could not find command #{task.inspect} in #{namespace.inspect} namespace."
-      else
-        raise UndefinedTaskError, "Could not find command #{task.inspect}."
-      end
-    end
-
-    def self.exit_on_failure?
-      true
-    end
-
     def api
       @api ||= EY::CLI::API.new
     end
@@ -106,7 +40,7 @@ module EY
         api.apps.match_one!(app_name)
       else
         api.app_for_repo!(repo)
-      end      
+      end
     end
 
     def get_apps(all_apps = false)
@@ -116,5 +50,53 @@ module EY
         [api.app_for_repo(repo)].compact
       end
     end
+
+  end # UtilityMethods
+
+  class Thor < ::Thor
+    include UtilityMethods
+
+    no_tasks do
+      def self.subcommand_help(cmd)
+        desc "#{cmd} help [COMMAND]", "Describe all subcommands or one specific subcommand."
+        class_eval <<-RUBY
+          def help(*args)
+            if args.empty?
+              EY.ui.say "usage: #{banner_base} #{cmd} COMMAND"
+              EY.ui.say
+              subcommands = self.class.printable_tasks.sort_by{|s| s[0] }
+              subcommands.reject!{|t| t[0] =~ /#{cmd} help$/}
+              EY.ui.print_help(subcommands)
+              EY.ui.say self.class.send(:class_options_help, EY.ui)
+              EY.ui.say "See #{banner_base} #{cmd} help COMMAND" +
+                " for more information on a specific subcommand." if args.empty?
+            else
+              super
+            end
+          end
+        RUBY
+      end
+
+      def self.banner_base
+        "ey"
+      end
+
+      def self.banner(task, task_help = false, subcommand = false)
+        scmd = EY::Thor.subcommands.invert[self]
+        task = (task_help ? task.formatted_usage(self, false, subcommand) : task.name)
+        [banner_base, scmd, task].compact.join(" ")
+      end
+
+      def self.handle_no_task_error(task)
+        raise UndefinedTaskError, "Could not find command #{task.inspect}."
+      end
+    end
+
+    protected
+
+    def self.exit_on_failure?
+      true
+    end
+
   end
 end
