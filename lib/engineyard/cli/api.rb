d6c5f94510626f461389b2b438180bb4d66cd204
Martin Emde <martin.emde@gmail.com>
2011-12-12 17:26:36 -0800

    Load and test API when it's used.

--- lib/engineyard/cli/api.rb
+++ lib/engineyard/cli/api.rb
@@ -5,33 +5,6 @@ require 'engineyard/eyrc'
 module EY
   class CLI
     class API
-      attr_reader :token
-
-      def initialize(token = nil)
-        @token = token
-        if ENV['ENGINEYARD_API_TOKEN']
-          @token = ENV['ENGINEYARD_API_TOKEN']
-        end
-        @token ||= EY::EYRC.load.api_token
-        @token ||= self.class.authenticate
-        raise EY::Error, "Sorry, we couldn't get your API token." unless @token
-        @api = EY::APIClient.new(@token)
-      end
-
-      def request(*args)
-        begin
-          @api.request(*args)
-        rescue EY::APIClient::InvalidCredentials
-          EY.ui.warn "Credentials rejected; please authenticate again."
-          refresh
-          retry
-        end
-      end
-
-      def refresh
-        @token = self.class.authenticate
-      end
-
       def self.authenticate
         EY.ui.info("We need to fetch your API token; please log in.")
         begin
@@ -39,10 +12,27 @@ module EY
           password = EY.ui.ask("Password: ", true)
           token = EY::APIClient.authenticate(email, password)
           EY::EYRC.load.api_token = token
+          token
         rescue EY::APIClient::InvalidCredentials
           EY.ui.warn "Invalid username or password; please try again."
           retry
         end
+      end
+
+      attr_reader :token
+
+      def initialize(endpoint)
+        EY::APIClient.endpoint = endpoint
+
+        @token = ENV['ENGINEYARD_API_TOKEN'] if ENV['ENGINEYARD_API_TOKEN']
+        @token ||= EY::EYRC.load.api_token
+        @token ||= self.class.authenticate
+
+        unless @token
+          raise EY::Error, "Sorry, we couldn't get your API token."
+        end
+
+        @api = EY::APIClient.new(@token)
       end
 
       protected
