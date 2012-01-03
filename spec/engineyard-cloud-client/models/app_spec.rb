79fd7fd90377d8d0648b9150040a4342f1d64e2e
Dr Nic Williams <drnicwilliams@gmail.com>
2012-01-02 16:27:04 -0800

    reformat spec

--- spec/engineyard-cloud-client/models/app_spec.rb
+++ spec/engineyard-cloud-client/models/app_spec.rb
@@ -4,11 +4,7 @@ describe "EY::CloudClient::App.create" do
   it "hits the create action in the API" do
     account = EY::CloudClient::Account.new(ey_api, {:id => 1234, :name => 'myaccount'})
 
-    FakeWeb.register_uri(
-      :post,
-      "https://cloud.engineyard.com/api/v2/accounts/1234/apps",
-      :body => ''
-    )
+    FakeWeb.register_uri(:post, "https://cloud.engineyard.com/api/v2/accounts/1234/apps", :body => '')
 
     app = EY::CloudClient::App.create(ey_api, account, 'myapp2', 'git@github.com:myaccount/myapp2.git', 'rails3')
 
