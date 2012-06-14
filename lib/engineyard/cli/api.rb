df73b7fabe0c623e8948196bb6f5e25b2a5269dd
Martin Emde <martin.emde@gmail.com>
2012-06-13 18:31:17 -0700

    Give better feedback when authentication fails

--- lib/engineyard/cli/api.rb
+++ lib/engineyard/cli/api.rb
@@ -25,12 +25,21 @@ module EY
         @ui = ui
         EY::CloudClient.endpoint = endpoint
 
-        @token = token # specified on command line
-        @token ||= ENV['ENGINEYARD_API_TOKEN'] if ENV['ENGINEYARD_API_TOKEN']
-        @token ||= EY::EYRC.load.api_token
-        @token ||= self.class.authenticate(ui)
-
-        unless @token
+        # If the token is specified, we don't ask for authentication when
+        # the token is reject, we just exit.
+        if @token = token
+          @specified = true
+          @source = 'token from --api-token'
+        elsif @token = ENV['ENGINEYARD_API_TOKEN']
+          @specified = true
+          @source = 'token from $ENGINEYARD_API_TOKEN'
+        elsif @token = EY::EYRC.load.api_token
+          @specified = false
+          @source = "api_token in #{EY::EYRC.load.path}"
+        elsif @token = self.class.authenticate(ui)
+          @specified = false
+          @source = 'credentials'
+        else
           raise EY::Error, "Sorry, we couldn't get your API token."
         end
 
@@ -50,10 +59,15 @@ module EY
           super
         end
       rescue EY::CloudClient::InvalidCredentials
-        ui.warn "Authentication failed."
-        @token = self.class.authenticate(@ui)
-        @api = EY::CloudClient.new(@token, @ui)
-        retry
+        if @specified
+          ui.error "Authentication failed: Invalid #{@source}."
+          exit(255)
+        else
+          ui.warn "Authentication failed: Invalid #{@source}. Retrying..."
+          @token = self.class.authenticate(@ui)
+          @api = EY::CloudClient.new(@token, @ui)
+          retry
+        end
       end
 
     end
