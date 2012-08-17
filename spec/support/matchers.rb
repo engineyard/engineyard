ca71841b5dc3322c90b801eb4858520bb7717a19
Martin Emde <martin.emde@gmail.com>
2012-08-17 13:15:13 -0700

    Remove unused matcher

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -14,15 +14,3 @@ RSpec::Matchers.define :have_command_like do |regex|
     "Found unwanted command:\n\n#{@found}\n\n(matches regex #{regex})"
   end
 end
-
-RSpec::Matchers.define :have_app_code do
-  match { |instance| instance.has_app_code? }
-
-  failure_message_for_should do |instance|
-    "Expected #has_app_code? to be true on instance: #{instance.inspect}"
-  end
-
-  failure_message_for_should_not do |instance|
-    "Expected #has_app_code? to be false on instance: #{instance.inspect}"
-  end
-end
