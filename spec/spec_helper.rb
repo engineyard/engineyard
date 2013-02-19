5cf9ee82522e419c8997eda6caafa3c1f198cf3a
Martin Emde <martin.emde@gmail.com>
2013-02-19 11:59:09 -0800

    Remove spec run noise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,7 +40,6 @@ TMPDIR = Pathname.new(__FILE__).dirname.parent.join('tmp')
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
 
   config.include SpecHelpers
   config.include SpecHelpers::IntegrationHelpers
